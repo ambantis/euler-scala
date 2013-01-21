4b9fa184ed950380508974a89d1e717f3ebdcf65
Alexandros Bantis <ambantis@gmail.com>
2013-01-21 14:11:04 -0800

    Solved problems 2 & 3

--- src/test/scala/net/euler/ProblemsTo010Suite.scala
+++ src/test/scala/net/euler/ProblemsTo010Suite.scala
@@ -1,9 +1,7 @@
-package net.projecteuler
+package net.euler
 
 import net.euler.ProblemsTo010._
 import org.scalatest._
-
-
 
 /**
  * 
@@ -13,9 +11,38 @@ import org.scalatest._
  */
 class ProblemsTo010Suite extends FlatSpec {
 
-  "A sum of Multiples 3 and 5 to 10" should "equal 23" in {
-    val result = getSumOfMultiplesTo(3, 5, 10)
+  "A sum of Multiples 3 and 5 to 10 [using recursion]" should "equal 23" in {
+    val result = getSumOfMultiplesA(3, 5, 10)
     assert(result === 23)
   }
 
+  "A sum of Multiples 3 and 5 to 10 [using fold]" should "equal 23" in {
+    val result = getSumOfMultiplesB(3, 5, 10)
+    assert(result === 23)
+  }
+
+  "A sum of Multiples 3 and 5 to 10 [using for]" should "equal 23" in {
+    val result = getSumOfMultiplesC(3, 5, 10)
+    assert(result === 23)
+  }
+
+  "A sum of Multiples 3 and 5 to 10 [using alt fold]" should "equal 23" in {
+    val result = getSumOfMultiplesD(3, 5, 10)
+    assert(result === 23)
+  }
+
+  "A fibonacci sequence up to 10" should "equal 1, 2, 3, 5, 8" in {
+    val result = fibo(10)
+    assert(result === List(1,2,3,5,8))
+  }
+
+  "A sum of all even fibonnaci numbers less than 100" should "equal 44" in {
+    val result = sumEvenFibonacciA(100)
+    assert(result === 44)
+  }
+
+  "The largest prime factor of 13195" should "equal 29" in {
+    val result = largestPrimeFactor(13195)
+    assert(result === 29)
+  }
 }
