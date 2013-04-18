a2e2463b4e36fb112b202b64537b7a4510676340
Alexandros Bantis <ambantis@gmail.com>
2013-04-17 22:49:20 -0700

    begin problem 28

---
+++ src/main/scala/net/euler/Problem027.scala
@@ -0,0 +1,34 @@
+package net.euler
+
+/**
+ * Quadratic primes
+ *
+ * Euler published the remarkable quadratic formula
+ *           n**2 + n + 41
+ *
+ * It turns out that the formula will produce 40 primes for the consecutive
+ * values n = 40, 40**2 + 40 + 41 = 40(40 + 1) + 41 is divisible by 41, and
+ * certainly when n = 4, 41**2 + 41 + 41 is clearly divisible by 41.
+ *
+ * Using computers, the incredible formula n**2 - 79n + 1601 was discovered,
+ * which produces 80 primes for the consecutive values n = 0 to 79. The
+ * product of the coefficients, -79 and 1601, is -126,479.
+ *
+ * Considering the quadratics of the form:
+ *           n**2 + an + b, where |a| < 1000 and |b| < 1000
+ *           where |n| is the modulus/absolute value of n
+ *           e.g., |11| = 11 and |-4| = 4
+ *
+ * Find the product of the coefficients, a and b, for the quadratic equation
+ * that produces the maximum number of primes for the consecutive values of n,
+ *
+ *
+ * User: Alexandros Bantis
+ * Date: 4/13/13
+ * Time: 1:52 PM
+ */
+object Problem027 {
+
+
+
+}
---
+++ src/main/scala/net/euler/Problem028.scala
@@ -0,0 +1,25 @@
+package net.euler
+
+/**
+  * Number spiral diagonals
+  *
+  * Starting with the number 1 and moving to the right in a clockwise
+  * direction a 5 by 5 spiral is formed as follows:
+  *
+  *           21 22 23 24 25
+  *           20  7  8  9 10
+  *           19  6  1  2 11
+  *           18  5  4  3 12
+  *           17 16 15 14 13
+  *
+  * It can be verified that the sum of the numbers on the diagonals is 101.
+  * What is the sum of the numbers on the diagonals in a 1001 by 1001 spiral
+  * formed in the same way?
+  *
+  * User: Alexandros Bantis
+  * Date: 4/17/13
+  * Time: 10:47 PM
+  */
+object Problem028 {
+
+}
